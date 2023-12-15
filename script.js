64ef4c0a2545d6714463b3226294cb7afa847557
sjaworowicz <125sjaworowicz@frhsd.com>
2023-12-15 08:05:08 -0500

    Video 5

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var titles = document.getElementsByClassName('title');
+var books = document.querySelectorAll('#book-list li .name');
 
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles)));
+Array.from(books).forEach(function(book){
+    book.textContent += '(book title)';
+});
 
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
+const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
