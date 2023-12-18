0dfaff3907e7d5add348a6f8e249fbdbf4798cd0
sjaworowicz <125sjaworowicz@frhsd.com>
2023-12-18 09:01:36 -0500

    Video 9

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const bookList = document.querySelector('#book-list');
+var btns = document.querySelectorAll('#book-list.delete');
 
-console.log('book-list next sibling is:', bookList.nextSibling);
-console.log('book-list next element sibling is:', bookList.nextElementSibling);
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e) {
 
-console.log('book-list previous sibling is:', bookList.previousSibling);
-console.log('book-list previous element sibling is:', bookList.previousElementSibling);
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li)
 
-bookList.previousElementSibling.querySelector('p')
+    });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('navigation to ', e.target.textContent, ' was prevented');
+});
