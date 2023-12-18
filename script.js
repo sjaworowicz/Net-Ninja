d7449fc10ea9416228f0b544ca7f068ca4ffc58b
sjaworowicz <125sjaworowicz@frhsd.com>
2023-12-18 09:18:37 -0500

    Video 10

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-var btns = document.querySelectorAll('#book-list.delete');
+const list = document.querySelector('#book-list ul');
 
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click', function(e) {
-
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li)
-
-    });
-});
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('navigation to ', e.target.textContent, ' was prevented');
+// delete books
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.tagret.parentElement;
+        li.parentNode.removeChild(li);
+    }
 });
