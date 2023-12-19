5a046c37aeb2a32dd1035b1e85a92ccca81e999e
sjaworowicz <125sjaworowicz@frhsd.com>
2023-12-19 10:27:10 -0500

    Video 13

--- script.js
+++ script.js
@@ -23,9 +23,14 @@ addForm.addEventListener('submit', function(e){
     deleteBtn.textContent = 'delete';
     bookName.textContent = value;
 
+    // add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
     // append to document
     li.appendChild(bookName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
+
 
 });
