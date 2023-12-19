f49d6bcaf4a62dc7fe066ba731a79109fc34d136
sjaworowicz <125sjaworowicz@frhsd.com>
2023-12-19 10:12:52 -0500

    Video 11

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ list.addEventListener('click', function(e){
         li.parentNode.removeChild(li);
     }
 });
+
+// add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+});
