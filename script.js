d35f7d109cdad460761a348718364e7de792702e
sjaworowicz <125sjaworowicz@frhsd.com>
2023-12-21 08:01:08 -0500

    Video 17

--- script.js
+++ script.js
@@ -59,3 +59,19 @@ book.style.display = 'block';
 }
 });
 });
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e){
+if(e.target.tagName == "LI"){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panels.forEach(function(panel){
+        if(panel == targetPanel){
+        panel.classList.add('active');
+    } else{
+        panel.classList.remove('active');
+    }
+});
+}
+});
